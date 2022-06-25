45b24e7b29aed346b4afbb912ab7b681814152b0
Juan Martin Campano <juan.martin.campano@gmail.com>
2022-06-25 10:44:06 -0300

    agrega event listener a las cells, click para borrar, boton de clear

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const container = document.getElementById('container');
 
 
 const grid = function(){
-    for(i = 0 ; i < 16 ; i++){
+    for(i = 0 ; i < 1056 ; i++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.addEventListener('mouseover',function (){cell.classList.add("hover");});
+        cell.addEventListener('click',function (){cell.classList.remove("hover");});
         container.appendChild(cell);
     }
 }
 grid();
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click',()=>window.location.reload());
+
